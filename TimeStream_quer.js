289b5a33721b9f44b9aa49ed94d9c154ede93684
Syed Arsalan <sya@4fo.de>
2023-05-17 15:48:02 +0200

    TimeStream_Query_SDK

---
+++ TimeStream_quer.js
@@ -0,0 +1,29 @@
+//import {TimestreamQueryClient, QueryCommand} from '@aws-sdk/client-timestream-query';
+//run npm install @aws-sdk/client-timestream-query first
+
+
+const {TimestreamQueryClient} = require("@aws-sdk/client-timestream-query");
+const {QueryCommand} = require("@aws-sdk/client-timestream-query");
+ const CREDENTIALS = {
+    accessKeyId: 'AKIAXTGD5IES42XZ2XET',
+    secretAccessKey: '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',
+  };
+
+
+  async function queryTimestream() {
+
+  const client = new TimestreamQueryClient({region: "eu-central-1", credentials: CREDENTIALS});
+  const query = 'SELECT * FROM "myDataStreamDB"."Cloud_Test_Table_4" WHERE time between ago(30m) and now() ORDER BY time DESC LIMIT 10';
+
+  const command = new QueryCommand({QueryString: query});
+
+  try {
+    const response = await client.send(command);
+    console.log("Query Results: " , response.Rows);
+  }
+  catch(err){
+    console.log("Error: ", err);
+  }
+}
+
+queryTimestream();
